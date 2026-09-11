d235cef798bf639f56e1642df7ed1d7f6400d992
Aryan-Pericharan <axp1295@case.edu>
2026-09-11 16:00:21 -0400

    Update touch.cpp

--- Lab 3/src/touch.cpp
+++ Lab 3/src/touch.cpp
@@ -2,15 +2,13 @@
 //Put your potentiometer pin assignment here
 const int sensorPin = A1;
 const int ledPin = 13;
-// axp1295: designed the led that should turn on and off in the rest of the code 
+// axp1295: designed the led that should turn on as 13 which is the onboard led on the ESP32
 
 void setup() {
   Serial.begin(115200);
   pinMode(sensorPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  // axp1295: sets the led pin as an output pin so that instructions may be told to it and not read from it 
-}
-
+  // axp1295: sets the led pin as an output pin so that instructions may be sent to it
 void loop() {
 
   //This line should print out the values from the sensor by Serial.println(??);
@@ -20,11 +18,12 @@ void loop() {
   Serial.println("Touch Detected");
 
   digitalWrite(ledPin, HIGH);
-  // axp1295: instructing the led to turn on(HIGH)
+  // axp1295: instructing the led to turn on(HIGH), turning it on when touch is detected 
  
   }else{
     Serial.println("Touch Not Detected");
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW)
+    // axp1295: opposite of the HIGH line. It turns off the LED upon no touch
   }
   delay(50);
 }
